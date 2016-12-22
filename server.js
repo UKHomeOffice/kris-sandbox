2f08a22eb498f5e6d89f10925c5300eccdc6b486
Kris Coverdale <kris.coverdale@gmail.com>
2016-12-22 15:28:58 +0000

    refactored to add index.js as master file

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 var http = require("http");
 
-http.createServer(function(request, response){
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    console.log("request received");
-    response.end();
-}).listen(8888);
+function start() {
+    function onRequest(request, response){
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write("Hello World");
+        console.log("request received");
+        response.end();
+    }
+
+    http.createServer(onRequest).listen(8888);
+    console.log("Server has started");
+}
+
+exports.start = start;
+
